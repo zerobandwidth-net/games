81ebceb9f497ea13c50c22c6d59ecb4a5749626a
zerobandwidth <pasha.paterson@gmail.com>
2017-11-26 23:08:58 -0500

    (#1) Fixed a warning. Added Eclipse project paths to `.gitignore`.

--- src/test/java/net/zerobandwidth/games/cards/euchre/EuchreDeckTest.java
+++ src/test/java/net/zerobandwidth/games/cards/euchre/EuchreDeckTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import net.zerobandwidth.games.cards.standard.PlayingCard;
 import net.zerobandwidth.games.cards.standard.PlayingCardDeckTest;
 
 import static net.zerobandwidth.games.cards.standard.PlayingCards.ACE ;
